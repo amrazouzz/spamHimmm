18dd80532c5021431923e09c25c1b1e15bb55632
amrazouzz <amrsalahazouz@gmail.com>
2022-12-12 18:46:03 +0200

    For the Hackers

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui as gui
+import time
+
+
+number = input("How many messages ya raye2: ")
+
+time.sleep(5)
+
+for i in range(int(number)):
+    message = 'eb3at l swar ya 7ywan'
+    m2 = "please ^_^"
+
+    gui.typewrite(message)
+    gui.press('Enter')
+    gui.typewrite(m2)
+    gui.press('Enter')
